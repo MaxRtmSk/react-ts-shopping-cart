fec9342de5fd8cc25ab1c3dfb4ee0707cb7770ad
Max <max.work.dev@gmail.com>
2021-05-27 19:58:56 +0300

    feat: add logic to handleRemoveFromCart func

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,17 @@ const App = () => {
     })
   };
 
-  const handleRemoveFromCart = () => {
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => (
+      prev.reduce((ack, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, { ...item, amount: item.amount - 1 }]
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    ))
   };
 
 
