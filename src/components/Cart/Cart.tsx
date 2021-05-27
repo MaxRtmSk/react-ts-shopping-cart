a998731699bb223337d7a2471a6cd288840bd080
Max <max.work.dev@gmail.com>
2021-05-27 20:06:12 +0300

    feat(Cart): add Total sum

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -12,19 +12,26 @@ type Props = {
   removeFromCart: (id: number) => void
 }
 
-const Cart: FC<Props> = ({ cartItems, addToCart, removeFromCart }) => (
-  <Wrapper>
-    <h2>Your Shopping Cart</h2>
-    {cartItems.length === 0 ? <p>No items</p> : null}
-    {cartItems.map(item => (
-      <CartItem
-        key={item.id}
-        item={item}
-        addToCart={addToCart}
-        removeFromCart={removeFromCart}
-      />
-    ))}
-  </Wrapper>
-)
+const Cart: FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calcTotal = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount * item.price, 0)
+
+
+  return (
+    <Wrapper>
+      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <p>No items</p> : null}
+      {cartItems.map(item => (
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+      <h2>Total: ${calcTotal(cartItems).toFixed(2)}</h2>
+    </Wrapper>
+  )
+}
 
 export default Cart
