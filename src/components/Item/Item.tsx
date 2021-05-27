1cbab948e19f7a5af199a3300d7e3fb7fbef9770
Max <max.work.dev@gmail.com>
2021-05-27 09:15:49 +0300

    feat: add Item

---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import Button from '@material-ui/core/Button';
+// Types
+import { CartItemType } from '../../App';
+// Styles
+import { Wrapper } from './Item.styles';
+
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+const Item: FC<Props> = ({ item, handleAddToCart }) => (
+  <Wrapper>
+    <img src={item.image} alt={item.title} />
+    <div>
+      <h3>{item.title}</h3>
+      <p>{item.description}</p>
+      <h3>${item.price}</h3>
+      <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
+    </div>
+  </Wrapper>
+)
+
+export default Item;
